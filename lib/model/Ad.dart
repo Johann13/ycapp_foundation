128bb8c7d25ff3429b38d2f0b5906ce5eedc6ab1
Johann Feser <johannfeser@gmail.com>
2020-04-14 22:53:23 +0200

    Create Ad.dart

---
+++ lib/model/Ad.dart
@@ -0,0 +1,29 @@
+import 'package:ycapp_foundation/model/base_model.dart';
+
+class Ad extends BaseModel {
+  String _id;
+  String name;
+  int day;
+  List<String> images;
+
+  Ad(this._id, this.name, this.day, this.images);
+
+  Ad.fromMap(Map map)
+      : this(
+          map['id'],
+          map['name'],
+          map['day'],
+          map['images'],
+        );
+
+  @override
+  String get id => _id;
+
+  @override
+  Map toJson() => {
+        'id': _id,
+        'name': name,
+        'day': day,
+        'images': images,
+      };
+}
