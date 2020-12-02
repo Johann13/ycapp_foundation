8472053f4c96d000b816ede58205630b1cf4b3be
Johann Feser <johannfeser@gmail.com>
2020-12-02 01:14:21 +0100

    Update schedule_colors.dart

--- lib/ui/schedule_colors.dart
+++ lib/ui/schedule_colors.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:flutter/painting.dart';
@@ -11,50 +12,51 @@ class ScheduleColors {
   );
 
   static List<Color> yellow = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 45, 98 / 100, 99 / 100),
-    HSVColor.fromAHSV(1, 22, 97 / 100, 96 / 100),
+    HSVColor.fromAHSV(1, 45, 99 / 100, 98 / 100),
+    HSVColor.fromAHSV(1, 23, 100 / 100, 96 / 100),
   );
 
   static List<Color> green = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 74, 90 / 100, 69 / 100),
-    HSVColor.fromAHSV(1, 93, 96 / 100, 46 / 100),
+    HSVColor.fromAHSV(1, 74, 92 / 100, 78 / 100),
+    HSVColor.fromAHSV(1, 93, 95 / 100, 46 / 100),
   );
 
   static List<Color> darkOrange = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 39, 99 / 100, 96 / 100),
-    HSVColor.fromAHSV(1, 14, 100 / 100, 89 / 100),
+    HSVColor.fromAHSV(1, 30, 98 / 100, 91 / 100),
+    HSVColor.fromAHSV(1, 11, 100 / 100, 79 / 100),
   );
 
   static List<Color> cyan = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 176, 95 / 100, 83 / 100),
-    HSVColor.fromAHSV(1, 170, 98 / 100, 57 / 100),
+    HSVColor.fromAHSV(1, 177, 94 / 100, 82 / 100),
+    HSVColor.fromAHSV(1, 170, 100 / 100, 57 / 100),
   );
 
   static List<Color> pink = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 314, 62 / 100, 88 / 100),
-    HSVColor.fromAHSV(1, 332, 85 / 100, 72 / 100),
+    HSVColor.fromAHSV(1, 315, 62 / 100, 88 / 100),
+    HSVColor.fromAHSV(1, 333, 86 / 100, 72 / 100),
   );
 
   static List<Color> lightOrange = getScheduleColorPallet(
-    HSVColor.fromAHSV(1, 29, 100 / 100, 92 / 100),
-    HSVColor.fromAHSV(1, 11, 99 / 100, 79 / 100),
+    HSVColor.fromAHSV(1, 40, 98 / 100, 94 / 100),
+    HSVColor.fromAHSV(1, 12, 97 / 100, 90 / 100),
   );
 
-  static List<Color> getScheduleColorPallet(HSVColor from, HSVColor to) {
-    double h = ((to.hue - from.hue) / 12);
-    double s = ((to.saturation - from.saturation) / 12);
-    double v = ((to.value - from.value) / 12);
-    print('diffs, h:$h,s:$s,v:$v');
-    print('from, h:${from.hue},s:${from.saturation},v:${from.value}');
-    print('to, h:${to.hue},s:${to.saturation},v:${to.value}');
+  static List<Color> getScheduleColorPallet(HSVColor from, HSVColor to,
+      [int split = 12]) {
+    double h = double.parse(((to.hue - from.hue) / split).toStringAsFixed(2));
+    double s = double.parse(
+        ((to.saturation - from.saturation) / split).toStringAsFixed(2));
+    double v =
+        double.parse(((to.value - from.value) / split).toStringAsFixed(2));
     List<Color> l = [];
 
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < split; i++) {
       Color c = HSVColor.fromAHSV(
         1,
-        from.hue + (h * i),
-        from.saturation + (s * i),
-        from.value + (v * i),
+        double.parse((from.hue + (h * i)).toStringAsFixed(2)).roundToDouble(),
+        math.min(
+            double.parse((from.saturation + (s * i)).toStringAsFixed(2)), 1.0),
+        math.min(double.parse((from.value + (v * i)).toStringAsFixed(2)), 1.0),
       ).toColor();
       l.add(c);
     }
